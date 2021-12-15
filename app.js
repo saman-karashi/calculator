4ca9107d9978473ca793b52831960800def3f3cc
Saman <saman.rampage@gmail.com>
2021-12-15 11:07:45 -0800

    Completed ui

---
+++ app.js
@@ -0,0 +1,11 @@
+//Get elements from the DOM;
+const evalutionEl=document.getElementById('evaluation'),
+      sumEl=document.getElementById('sum'),
+      operatorsEl=document.querySelectorAll('.operator'),
+      numbersKeyEl=document.querySelectorAll('.number-key')
+
+
+
+
+
+
