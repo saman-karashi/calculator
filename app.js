d697e04513582d3e7cc91ed976515994afcf6f12
Saman <saman.rampage@gmail.com>
2021-12-22 13:17:10 -0800

    Done  number from positive to negative

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 const evaluationEl = document.getElementById('evaluation'),
   sumEl = document.getElementById('sum'),
   operatorsEl = document.querySelectorAll('.operator'),
+  equalEl = document.querySelector('.equal'),
+  dotEL = document.querySelector('.dot'),
+  plusMinusEl = document.querySelector('.plus-minus'),
   numbersKeyEl = document.querySelectorAll('.number-key'),
-  clearBtn = document.querySelector('button[data-clear=AC]');
+  clearBtn = document.querySelector('.clear');
 
 //Storing left && right operand
 let leftOperand = '',
@@ -15,93 +18,10 @@ let operatorStore;
 //Storing sum value
 let result;
 
-//EventListeners
-const keyNumbersHandler = e => {
-  //Check operatorStore value
-  if (!operatorStore) {
-    leftOperand += e.target.textContent;
-  } else {
-    rightOperand += e.target.textContent;
-  }
+let index = -1;
 
-  leftOperand
-    ? (evaluationEl.value = leftOperand)
-    : rightOperand
-    ? (evaluationEl.value = rightOperand)
-    : null;
-
-  calculate();
-};
-
-//Calculate function
-function calculate() {
-  //Convert from string to number
-  const toNumberLeftOperand = Number(leftOperand);
-  const toNumberRightOperand = Number(rightOperand);
-
-  switch (operatorStore) {
-    case '+':
-      return (result = toNumberLeftOperand + toNumberRightOperand);
-      break;
-    case 'x':
-      return (result = toNumberLeftOperand * toNumberRightOperand);
-      break;
-    case '-':
-      return (result = toNumberLeftOperand - toNumberRightOperand);
-      break;
-    case '÷':
-      return (result = toNumberLeftOperand / toNumberRightOperand);
-      break;
-    case '%':
-      return (result = toNumberLeftOperand % toNumberRightOperand);
-      break;
-    default:
-      break;
-  }
-}
-
-function equalHandler() {
-  if (operatorStore === '=') {
-    evaluationEl.value = '';
-  }
-}
-
-const operatorHandler = e => {
-  if (leftOperand) {
-    operatorStore = e.target.textContent;
-    evaluationEl.value = operatorStore;
-  }
-
-  if (result) {
-    sumEl.innerHTML = result;
-    leftOperand = result;
-    rightOperand = '';
-  }
-
-  equalHandler();
-};
-
-//DOM loaded focus on input
-window.addEventListener('DOMContentLoaded', () => {
-  evaluationEl.focus();
-  evaluationEl.value = '';
-  sumEl.innerHTML = '';
-});
-
-//Clear input and all variables
-clearBtn.addEventListener('click', () => {
-  evaluationEl.value = '';
-  leftOperand = '';
-  rightOperand = '';
-  operatorStore = '';
-  result = '';
-  sumEl.innerHTML = '';
-});
-
-// calculator func
-calculator();
-
-function calculator() {
+//Event Listeners
+function eventListeners() {
   //Loop over key-numbers
   numbersKeyEl.forEach(numberKey => {
     numberKey.addEventListener('click', keyNumbersHandler);
@@ -110,4 +30,102 @@ function calculator() {
   operatorsEl.forEach(operator => {
     operator.addEventListener('click', operatorHandler);
   });
+
+  //Equal eventHandler
+  equalEl.addEventListener('click', equalHandler);
+
+  //PlusMinus eventHandler
+  plusMinusEl.addEventListener('click', plusMinusHandler);
+
+  //DOM loaded focus on input
+  window.addEventListener('DOMContentLoaded', () => {
+    evaluationEl.focus();
+    evaluationEl.value = '';
+    sumEl.innerHTML = '';
+  });
+
+  //Clear input and all variables
+  clearBtn.addEventListener('click', () => {
+    evaluationEl.value = '';
+    leftOperand = '';
+    rightOperand = '';
+    operatorStore = '';
+    result = '';
+    sumEl.innerHTML = '';
+    index = -1;
+  });
+}
+
+eventListeners();
+
+//Calculate function
+function calculate() {
+  const toNumberLeftOperand = Number(leftOperand);
+  const toNumberRightOperand = Number(rightOperand);
+
+  switch (operatorStore) {
+    case '+':
+      return (result = toNumberLeftOperand + toNumberRightOperand);
+      break;
+    case '×':
+      return (result = toNumberLeftOperand * toNumberRightOperand);
+      break;
+    case '-':
+      return (result = toNumberLeftOperand - toNumberRightOperand);
+      break;
+    case '÷':
+      return (result = toNumberLeftOperand / toNumberRightOperand);
+      break;
+  }
+}
+
+//Number event handler
+function keyNumbersHandler(e) {
+  //Check operatorStore value
+  if (!operatorStore) {
+    leftOperand += e.target.textContent;
+    evaluationEl.value += e.target.textContent;
+  } else {
+    rightOperand += e.target.textContent;
+    evaluationEl.value += e.target.textContent;
+  }
+
+  calculate();
+}
+
+//Operator event handler
+function operatorHandler(e) {
+  if (leftOperand) {
+    operatorStore = e.target.textContent;
+    evaluationEl.value = leftOperand + operatorStore;
+  }
+
+  if (result) {
+    if (result == Infinity) return divisionHandler();
+    sumEl.innerHTML = result;
+    leftOperand = result;
+    evaluationEl.value = leftOperand + operatorStore;
+    rightOperand = '';
+  }
+}
+
+//Equal handler
+function equalHandler() {
+  result ? (sumEl.innerHTML = result) : (sumEl.innerHTML = '');
+  divisionHandler();
+}
+
+function divisionHandler() {
+  if (rightOperand === '0' && operatorStore === '÷') {
+    sumEl.innerHTML = 'Cannot divide by zero';
+  }
+}
+
+function plusMinusHandler() {
+  if (leftOperand && !operatorStore) {
+    leftOperand = '-'.concat(leftOperand);
+  } else if (rightOperand && operatorStore) {
+    rightOperand = '-'.concat(rightOperand);
+  }
+
 }
